a20f3ff2a0b54cc6f6828c068624efc57f54b579
Stefan Zhu <zhuzheyao7@gmail.com>
2020-02-09 17:53:36 -0500

    first commit

---
+++ TeamE_SensorMotor/TeamE_SensorMotor.ino
@@ -0,0 +1,278 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+// Pin Definitions
+const int IRpin = 1;                 // IR goes to Board A1
+const int IRbuttonPin = 2;          // IR button goes to Digital 2
+const int servoPin = 10;             // Servo pin goes to Digital 10
+const int stepperPinA = 8;
+const int stepperPinB = 9;
+const int ultraPin = 0;
+
+
+// State Changing Variables
+int IRbuttonVal = 0;
+const int numStates = 4;
+int state = 0;
+bool IRcanChange = true;
+int angle;
+int control = 0;
+int prev_motor = 0;
+int motor = 0;
+int sensor_reading;
+bool newAngle = false;
+
+// IR Sensor Variables
+int val = 0;                 // variable to store the values from sensor(initially zero)
+int pos = 0;                 // variable to store the servo position
+float IRhigh = 4;
+float IRlow = .5;
+
+// Stepper Motor Variables
+String readString;
+int StepCounter = 0;
+int Stepping = false;
+
+
+
+void setup()
+{
+  Serial.begin(9600);               // starts the serial monitor
+  myservo.attach(servoPin);  // attaches the servo on pin 10 to the servo object
+  pinMode(IRbuttonPin, INPUT);
+
+  pinMode(stepperPinA, OUTPUT);
+  pinMode(stepperPinB, OUTPUT);
+  digitalWrite(stepperPinB, LOW);
+  digitalWrite(stepperPinA, LOW);
+}
+
+void loop()
+{
+//  Serial.println("control:");/
+//  Serial.print("motor:"); 
+//  Serial.println(motor);
+//  Serial.print("control:");
+//  Serial.println(control);
+//  Serial.println("angle:");
+//  Serial.print(angle);
+
+
+  parseInput();
+
+  if(motor == 1) // Servo Motor Functions
+  {
+    if (control == 0){
+      report_state();
+      servoIR();   //Servo Sensor Control
+      } 
+    else if (control == 1 && newAngle) // Servo Serial Control
+    {
+//      Serial.println(newAngle);/
+      servoSerial();
+      newAngle = false;
+    }
+  }
+  
+  else if(motor == 2) // Stepper Motor Functions
+  {
+    if (control == 0) {
+      servoIR();   
+      ultrastepper();
+      } // Stepper Sensor Control
+    else if (control == 1 && newAngle)  // Stepper Serial Control
+    {
+      serialStepper();
+      newAngle = false;
+    }
+  }
+
+  else if(motor == 3) // DC Brushless Motor Functions
+  {
+    if (control == 0) Serial.println("Brushless Sensor Func"); // Brushless Sensor Control
+    else if (control == 1 && newAngle) // Brushless Serial Control
+    {
+      Serial.println("Brushless Sensor Func");
+      newAngle = false;
+    }
+
+  }
+}
+
+  ////////////////////////////////////////////////////////////////
+  /////////////////// Ultramotor Functions ///////////////////////
+  void ultrastepper()
+  {
+    int Ultrasensor, Ultrainches;
+
+    // read the analog output of the EZ1 from analog input 0
+    Ultrasensor = analogRead(ultraPin);
+    Ultrainches = Ultrasensor / 2;
+    sensor_reading = Ultrainches; 
+    //Serial.println(inches,DEC);
+    delay(1);
+    digitalWrite(stepperPinB, HIGH);
+    delay(Ultrainches / 10);
+    digitalWrite(stepperPinB, LOW);
+    delay(Ultrainches / 10);
+  }
+
+void serialStepper()
+{
+  int n = angle;
+  int m;
+  if (n >= -360 && n <= 360)
+  {
+        //Serial.println(n);
+    if (n >= 0 && n <= 360)
+    { //forward
+      m = n * 4.444;
+      Serial.println(m);
+      digitalWrite(stepperPinA, LOW);
+      Stepping = true;
+    }
+    else
+    { //reverse
+      m = -n * 4.444;
+      Serial.println(m);
+      digitalWrite(stepperPinA, HIGH);
+      Stepping = true;
+    }
+  }
+  while (Stepping == true)
+  {
+    digitalWrite(stepperPinB, HIGH);
+    delay(1);
+    digitalWrite(stepperPinB, LOW);
+    delay(1);
+    StepCounter = StepCounter + 1;
+  
+    if (StepCounter == m)
+    {
+      StepCounter = 0;
+      Stepping = false;
+    }
+  }
+}
+
+  ////////////////////////////////////////////////////////////////
+  /////////////////// Get IR/Servo Functions//////////////////////
+  void servoSerial()
+  { 
+    myservo.write(angle);
+    newAngle = false;
+  }
+
+  void servoIR()
+  {
+    int IRdist =  averageFilter();
+    int new_pos = setServo(IRdist);
+    sensor_reading = IRdist;
+    myservo.write(new_pos);
+  }
+
+
+  float getIRdist()
+  {
+    int IRdist = analogRead(IRpin);       // reads the value of the sharp sensor
+    //  Serial.println(val);
+    IRdist = (IRdist - 239.46) / 65.961;
+    if (IRdist < -1) IRdist = 5;
+    if (IRdist > IRhigh) IRdist = IRhigh;
+    if (IRdist < IRlow) IRdist = IRlow;
+
+    return IRdist;
+  }
+
+
+  float averageFilter()
+  {
+    float distance_filtered = 0;
+    int num_points = 200;
+    for (int i = 0; i < num_points; ++i)
+    {
+      distance_filtered += getIRdist();
+    }
+    delay(50);
+    distance_filtered /= (num_points);
+
+    return distance_filtered;
+  }
+
+  int setServo(int IRdist)
+  {
+    float servLow = 0;
+    float servHigh = 180;
+    float pos2 = servLow + (float(IRdist) - IRlow) * (servHigh - servLow) / (IRhigh - IRlow);
+    return  int(pos2);
+  }
+
+  ////////////////////////////////////////////////////////////////
+  /////////////////// State  Related functions ///////////////////
+  void getIRbuttonInfo()
+  {
+    IRbuttonVal = digitalRead(IRbuttonPin);
+    if (IRbuttonVal == HIGH && IRcanChange) {
+      state = buttonPress(state);
+      IRcanChange = false;
+      Serial.print("Entering State: ");
+      Serial.println(state % numStates);
+    }
+    if (IRbuttonVal == LOW) IRcanChange = true;
+    state = state % numStates;
+  }
+
+  int buttonPress(int val)
+  {
+    delay(100);
+    return val + 1;
+  }
+
+
+void parseInput()
+{
+  String serialIn = "";
+  // Get Input From Keyboard
+  if (Serial.available() > 0) serialIn = Serial.readString();
+  
+  // Check if full input is received
+  if (serialIn.length() == 4)
+  {
+    // Extract first char from input
+    //Serial.println(serialIn);
+  char inChar = serialIn.charAt(0);
+  serialIn.remove(0,1);
+    
+    // Check if remaining is a number
+    if(serialIn.toInt()|| serialIn=="000")
+    {
+      // Convert String
+      int newVal = serialIn.toInt();
+      
+      // Update State
+      if(inChar == 'm'){
+        prev_motor = motor;
+        motor = newVal;
+        if (newVal !=  prev_motor){
+          control = 0;
+        }
+       }
+       
+      else if(inChar == 'c') control = newVal;
+      else if(inChar == 'a') 
+      {
+        newAngle = true;
+        angle = newVal;
+      }
+   }
+  }
+}
+
+
+
+
+void report_state(){
+  Serial.print('s');
+  Serial.println(sensor_reading);
+}
---
+++ sensor_motor_GUI.py
@@ -0,0 +1,414 @@
+import random
+from tkinter import *
+import os 
+# from xls_write import xls_write
+import argparse
+# import imutils
+# import cv2 as cv
+# import PySpin
+import numpy as np
+# import matplotlib
+# import xlwt
+# from matplotlib import pyplot as plt
+# from Acquisition_Stefan import*
+import time
+import datetime
+# from cv_analysis import Analysis
+# from main import*
+# from PIL import ImageTk,Image 
+import serial
+import threading
+import shutil,sys
+
+EmptyGray = "gray30"
+# page_bool=[True,False]
+page_bool = [True, False, False, False]
+upper_dir="software_test/"  # the upper most directory under which the result of each session is stored in current path, which will be initialized later
+now = datetime.datetime.now()   #for getting current time 
+
+# Setting Up Serial Port 
+ser = serial.Serial('/dev/ttyACM0',9600, writeTimeout=0, parity=serial.PARITY_NONE, rtscts=False,dsrdtr=False)
+ser.bytesize=serial.EIGHTBITS
+ser.stopbits = serial.STOPBITS_ONE #number of stop bits
+ser.xonxoff = False    #disable software flow control
+
+
+# def motor_control(position):  #place holder      
+#     position=str(position)
+#     position_command=("P=%s"+"\r\n")%position
+#     if not ser.isOpen():
+#         ser.open()
+#     ser.write(('UDI'+'\r\n').encode("ascii"))
+#     ser.write(('UCI'+'\r\n').encode("ascii"))
+#     ser.write(('AMPS=1023'+'\r\n').encode("ascii"))
+#     ser.write(("MP"+"\r\n").encode("ascii"))
+#     ser.write(("ZS"+"\r\n").encode("ascii"))
+#     ser.write(("A=10000"+"\r\n").encode("ascii"))
+#     ser.write(("V=400000"+"\r\n").encode("ascii"))
+#     ser.write(position_command.encode("ascii"))
+#     ser.write(("G"+"\r\n").encode("ascii"))
+#     ser.write(("WHILE Bt"+"\r\n").encode("ascii"))
+#     ser.write(("LOOP"+"\r\n").encode("ascii"))
+#     ser.write(("BRKENG"+"\r\n").encode("ascii"))
+#     ser.write(("END"+"\r\n").encode("ascii"))
+
+
+
+def send_command_threading(msg):
+    t2 = threading.Thread(target=send_command, args=(msg))
+    t2.start()
+    t2.join()
+     
+def send_command(msg):
+    ser.write((msg).encode("ascii"))
+
+
+class App(object):
+    def __init__(self): pass
+    def mousePressed(self, event): pass
+    def keyPressed(self, event): pass
+    def timerFired(self): pass
+    def redrawAll(self): pass
+    def mouseMotion(self, event): pass
+    def mouseReleased(self, event): pass
+
+    # Call app.run(width,height) to get your app started
+    def run(self, width=1100, height=600):
+        # create the root and the canvas
+        root = Tk()
+        self.width = width
+        self.height = height
+        self.canvas = Canvas(root, width=width, height=height)
+        self.canvas.configure(background="SlateGray2")
+        # self.canvas.configure(background="LightSteelBlue4")
+        self.canvas.pack()
+
+
+        # set up events
+        def redrawAllWrapper():
+            self.canvas.delete(ALL)
+            self.redrawAll()
+            self.canvas.update()
+
+        def mousePressedWrapper(event):
+            self.mousePressed(event)
+            redrawAllWrapper()
+
+        def mouseMotionWrapper(event):
+            self.mouseMotion(event)
+            redrawAllWrapper()
+
+        def doubleClickedWrapper(event):
+            self.doubleClicked(event)
+            redrawAllWrapper()
+
+        def mouseReleasedWrapper(event):
+            self.mouseReleased(event)
+            redrawAllWrapper()
+
+        def keyPressedWrapper(event):
+            self.keyPressed(event)
+            redrawAllWrapper()
+
+        root.bind("<Button-1>", mousePressedWrapper)
+        root.bind("<Motion>", mouseMotionWrapper)
+        root.bind("<ButtonRelease>", mouseReleasedWrapper)
+        # root.bind("<Double-Button-1>", doubleClickedWrapper)
+        root.bind("<Key>", keyPressedWrapper)
+
+
+        # set up timerFired events
+        self.timerFiredDelay = 1 # milliseconds
+        def timerFiredWrapper():
+            self.timerFired()
+            redrawAllWrapper()
+            # pause, then call timerFired again
+            self.canvas.after(self.timerFiredDelay, timerFiredWrapper)
+
+        # init and get timerFired running
+        #self.__init__()
+        t1=threading.Thread(target=timerFiredWrapper)
+        t1.start()
+        # t1.join()
+        # and launch the app
+        root.mainloop()
+        ser.close()  #Close the serial port when the software is closed
+        print("Bye")
+
+
+
+class Interface(App):
+    def __init__(self):
+        App.__init__(self)
+        self.W=1100
+        self.H=600
+        self.Frontpage=Frontpage(self.W,self.H)
+        self.stepper_motor_page=motor_page(self.W,self.H,"stepper")
+        self.servo_motor_page=motor_page(self.W,self.H,"servo")
+        self.dc_brushless_page=motor_page(self.W,self.H, "dc")
+        self.page_list=[self.Frontpage,self.servo_motor_page,self.stepper_motor_page,self.dc_brushless_page]
+
+
+
+    def draw_page(self,canvas):
+        for i in range(len(self.page_list)):
+            # print(page_bool)
+            if page_bool[i]==True:
+                self.page_list[i].draw_page(canvas)
+        # print(page_bool)
+
+    def redrawAll(self):
+        canvas = self.canvas
+        self.draw_page(canvas)
+
+    def mousePressed(self,event):
+        for i in range(len(self.page_list)):
+                if page_bool[i]==True:
+                    self.page_list[i].mousePressed(event)
+
+    def keyPressed(self,event):
+        for i in range(len(self.page_list)):
+                if page_bool[i]==True:
+                    self.page_list[i].keyPressed(event)
+
+
+
+class Frontpage(object):
+    def __init__(self,width,height):
+        self.w=width
+        self.h=height
+        self.user_command=''
+        self.b1_loc = [self.w/2-120, self.h/2-200, self.w/2+180, self.h/2-110]
+        self.b2_loc = [self.w/2-120, self.h/2-90, self.w/2+180, self.h/2]
+        self.b3_loc = [self.w/2-120, self.h/2+20, self.w/2+180, self.h/2+110]
+        send_command('m%03d'%0)
+        pass
+
+    def draw_button(self,canvas):
+        canvas.create_rectangle(self.b1_loc[0], self.b1_loc[1], self.b1_loc[2], self.b1_loc[3],
+                                fill='khaki', width=0)
+        canvas.create_text((self.b1_loc[0]+self.b1_loc[2])/2, (self.b1_loc[1]+self.b1_loc[3])/2,fill="darkblue",font="Times 20 italic bold",text="Servo Motor")
+        
+        canvas.create_rectangle(self.b2_loc[0], self.b2_loc[1], self.b2_loc[2], self.b2_loc[3],
+                                fill='khaki', width=0)
+        canvas.create_text((self.b2_loc[0]+self.b2_loc[2])/2, (self.b2_loc[1]+self.b2_loc[3])/2,fill="darkblue",font="Times 20 italic bold",text="Stepper Motor")
+
+        canvas.create_rectangle(self.b3_loc[0], self.b3_loc[1], self.b3_loc[2], self.b3_loc[3],
+                                fill='khaki', width=0)
+        canvas.create_text((self.b3_loc[0]+self.b3_loc[2])/2, (self.b3_loc[1]+self.b3_loc[3])/2,fill="darkblue",font="Times 20 italic bold",text="DC brushless Motor")
+
+    def draw_page(self,canvas):
+        # canvas.create_rectangle(0, 0, 50, 60,
+        #                         fill='yellow', width=0)
+        self.draw_button(canvas)
+
+
+    def mousePressed(self,event):
+        x=event.x
+        y=event.y
+        global page_bool
+        if self.b1_loc[0]<=x<=self.b1_loc[2] and self.b1_loc[1]<=y<=self.b1_loc[3]:
+            page_bool = [False, True, False, False]
+            motor_msg = 'm%03d'%1
+            print('motor mode: Servo')
+            
+        elif self.b2_loc[0]<=x<=self.b2_loc[2] and self.b2_loc[1]<=y<=self.b2_loc[3]:
+            page_bool = [False, False, True, False]
+            motor_msg = 'm%03d'%2
+            print('motor mode: Stepper')
+            
+        elif self.b3_loc[0]<=x<=self.b3_loc[2] and self.b3_loc[1]<=y<=self.b3_loc[3]:
+            page_bool = [False, False, False, True]
+            motor_msg = 'm%03d'%3
+            print('motor mode: DC brushless')
+        
+        # control_msg ='c%03d'%1
+        # send_command(control_msg)
+        send_command(motor_msg)
+        return 
+
+
+
+class motor_page(object):
+    def __init__(self,width,height,motor_type):
+        self.motor_type = motor_type
+        self.width=width
+        self.height=height
+        self.button_width=150
+        self.button_height=50
+        self.motor_angle=''
+        self.sensor_reading = ''
+        self.user_command=''
+        self.label_box=False
+        self.lengnth_box=False
+        self.is_inspecting=False
+        self.buffer_path='buffer/'
+        self.abort=False
+        self.current_blade_length='8'
+        self.current_image=None
+        self.abort=False
+        self.control_modes = [True,False]      #indx 0: sensor control mode    indx 1: manual control mode
+        # self.t3=threading.Thread(target=self.receive_msg)
+        # self.t3.start()
+        # self.t3.join()
+        pass
+
+    def receive_msg(self):
+        sensor_reading = ser.readline()
+        sensor_reading= sensor_reading.decode('ASCII')
+        self.sensor_reading = sensor_reading[1:]
+        return 
+
+    def draw_page(self,canvas):
+        # self.receive_msg()
+        self.draw_button(canvas)
+        self.draw_massage(canvas)
+        self.draw_user_command(canvas)
+
+        canvas.create_image(0,0,image=self.current_image,anchor=NW)
+
+
+
+    def draw_button(self,canvas):
+        button_width=self.button_width
+        button_height=self.button_height
+
+
+        #Sensor Control Mode
+        if self.control_modes[0]:
+            button1_fill="SeaGreen1"
+
+        else:
+
+            button1_fill="Maroon"
+
+        canvas.create_rectangle(800, 200,800+button_width,200+button_height,
+                                fill=button1_fill, width=0)
+        canvas.create_text(800+button_width/2, 200+button_height/2,fill="darkblue",font="Times 10 italic bold",text="Sensor Control")
+       
+        #Manul Mode Button
+        if self.control_modes[1]:
+            button2_fill="SeaGreen1"
+        else:
+            button2_fill="Maroon"
+        
+        canvas.create_rectangle(800+button_width, 200, 800+2*button_width,200+button_height,
+                                fill=button2_fill, width=0)
+        canvas.create_text(800+button_width*1.5, 200+button_height/2,fill="darkblue",font="Times 10 italic bold",text="Manual Mode")
+
+
+        #send command button
+        text = "Send"
+
+        canvas.create_rectangle(800+button_width ,100, 800+2*button_width,100+button_height,
+                                fill="SeaGreen1", width=1)
+        canvas.create_text(800+button_width*1.5, 100+button_height/2,fill="darkblue",font="Times 10 italic bold",text=text)
+
+
+        #back Button
+        canvas.create_rectangle(self.width-100 ,self.height-50, self.width,self.height,
+                                fill="khaki", width=1)
+        canvas.create_text(self.width-50,self.height-25,fill="darkblue",font="Times 10 italic bold",text="Back")
+
+
+
+    def draw_massage(self,canvas):
+        canvas.create_rectangle(20,self.height-90,600,self.height-20,fill="white",width=0)
+        canvas.create_text(80,self.height-80,fill="darkblue",font="Times 10 italic bold",text="Sensor Reading:")
+        canvas.create_text(80,self.height-30,fill="black",font="Times 10 italic bold",text=self.sensor_reading)
+
+        canvas.create_rectangle(20,self.height-200,600,self.height-130,fill="white",width=0)
+        canvas.create_text(80,self.height-190,fill="darkblue",font="Times 10 italic bold",text="Motor Angle:")
+        canvas.create_text(80,self.height-180,fill="black",font="Times 10 italic bold",text=self.motor_angle)
+
+
+
+    def mousePressed(self,event):
+        x = event.x
+        y = event.y
+        button_width=self.button_width
+        button_height=self.button_height
+
+        #Sensor Control Mode
+        if 800<=x<=800+button_width and 200<=y<=200+button_height:
+            self.control_modes[1] = False
+            self.control_modes[0]= True
+            msg = 'c%03d'%0
+            send_command(msg)        
+            print(self.control_modes)
+
+        #Serial Control Mode
+        if 800+button_width<=x<=800+2*button_width and 200<=y<=200+button_height:
+            self.control_modes[1] = True
+            self.control_modes[0]= False
+            msg = 'c%03d'%1
+            # send_command(msg)  
+            send_command_threading(msg)
+            print(self.control_modes)
+
+
+        #Send Command Button
+        if 800+button_width<=x<=800+2*button_width and 100<=y<=100+button_height:
+            # self.hover_list[3]=True
+            # self.hover_list[2]=False
+            self.abort=True
+            if self.control_modes[1]:
+                msg = 'a%03d'% int(self.user_command)
+                send_command(msg)
+                print('sent msg%s'%msg)
+
+        #Back Button
+        if self.width-100<=x<=self.width and self.height-50<=y<=self.height:
+            global page_bool
+            page_bool[0]=True
+            page_bool = [True, False, False, False]
+            self.control_modes = [True,False] 
+            msg = 'm%03d'%0
+            send_command(msg)
+            print('front page')
+
+
+        #User command Input Box
+        if 1100-350<=x<=1100-10 and 50<=y<=90:
+            self.label_box=True
+        else:
+            self.label_box=False
+
+
+    def keyPressed(self,event):
+        if self.label_box==True:
+            self.user_command=self.user_command+event.char
+            if event.keysym=="BackSpace":
+                # print(self.user_command)
+                self.user_command=self.user_command[0:len(self.user_command)-2]
+     
+        if self.lengnth_box==True:
+            if (event.char=='1' or event.char=='2'\
+            or event.char=='3'or event.char=='4'or event.char=='5'or event.char=='6' or event.char=='7' \
+            or event.char=='8'or event.char=='9' or event.char=='0' ):    #making sure that user only inputs number for blade length
+                self.current_blade_length=self.current_blade_length+event.char
+            if event.keysym=="BackSpace":
+                self.current_blade_length=self.current_blade_length[0:len(self.current_blade_length)-2]
+
+
+    def draw_user_command(self,canvas):
+        canvas.create_rectangle(1100-350, 50, 1100-10, 90,
+                                fill='white', width=0)
+
+        canvas.create_text(1100-350, 70,fill="darkblue",anchor='w',font="Times 11 italic bold",text="User Command:")
+        canvas.create_text(1100-250, 70,fill="black",anchor='w',font="Times 11 italic bold",text=self.user_command)
+        if self.label_box:
+            canvas.create_line(1100-250+len(self.user_command)*7,50,1100-250+len(self.user_command)*7,80,fill='darkblue')     # this is the cursor
+    
+    
+
+
+
+
+
+
+        # super().draw_page(canvas)
+
+
+blade_inspector=Interface()
+blade_inspector.run()
